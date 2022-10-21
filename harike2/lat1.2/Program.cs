930616bd974e50c6a2a49982c7cef3939138df5a
jananggapratama16 <116141516+jananggapratama16@users.noreply.github.com>
2022-10-21 18:16:31 +0700

    Add files via upload

---
+++ harike2/lat1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace perulangan_for{
+    class programfor{
+        static void Main (String [] args){
+            int v1, v2 = 0;
+
+            Console.Write("input bilangan pertama :");
+            v1 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("input bilangan kedua :");
+            v2 = Convert.ToInt16(Console.ReadLine());
+
+            for (int pertama = v1; pertama < v2; pertama = pertama + 1){
+            Console.WriteLine("Hasil Pengulangan ke: {0}", pertama);
+            }
+            Console.ReadLine();
+        }
+    }
+}
