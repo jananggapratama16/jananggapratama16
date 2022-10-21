f8d43463785831c6bbb0c3cf9e3f4314fcfcb1c5
jananggapratama16 <116141516+jananggapratama16@users.noreply.github.com>
2022-10-21 18:23:22 +0700

    Add files via upload

---
+++ harike5/lat1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TipeArray{
+    class Array{
+        static void UjiCobaArray(int[,] Array){
+            for (int nAwal = 0; nAwal < Array.GetLength(0); nAwal++){
+                for (int nAkhir = 0; nAkhir < Array.GetLength(1); nAkhir++){
+                    System.Console.WriteLine ("Data Index Elemen Ke : ({0} , {1})={2}" , nAwal, nAkhir, Array[nAwal, nAkhir]);
+                }
+        
+            }
+        }
+        static void Main(string[] args){
+            UjiCobaArray(new int[,]{{1,5},{4,3},{7,1},{2,8},{3,3},{7,4},{3,1}});
+            System.Console.WriteLine("Tekan Bebas Untuk Keluar Halaman Kerja ! ");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ harike5/lat2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace InputanArray{
+    class DataInput{
+        static void Main(string[] args){
+            int JmlArray;
+            Console.WriteLine("=============================================");
+            Console.Write("Silahkan Input Banyak Junlah Array :");
+            JmlArray = Convert.ToInt16(Console.ReadLine());
+            // menentukan nilai inputan string
+            string? [] NamaTeman = new string[JmlArray];
+            // menentukan inputan data menjadi array
+            for(int nAwal=0; nAwal < JmlArray; nAwal++){
+                Console.Write("Silahkan Input Nama Teman Anda : ");
+                NamaTeman[nAwal] = Console.ReadLine();
+
+            }
+            Console.WriteLine("===================================================");
+            Console.WriteLine("Daftar Nama Teman Yang Sudah Diinput :");
+            for(int nIndex = 0; nIndex < JmlArray; nIndex++){
+                Console.WriteLine("Daftar Nama Teman : " + NamaTeman[nIndex]);
+            }
+            Console.WriteLine("=====================================================");
+            Console.ReadLine();
+
+        }
+    }
+}
