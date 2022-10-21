1530e0fda4bc688219705d788468075a1ce7715f
jananggapratama16 <116141516+jananggapratama16@users.noreply.github.com>
2022-10-21 18:24:39 +0700

    Add files via upload

---
+++ harike5.1/TUGASLAT2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace InputanArray{
+    class DataInput{
+        static void Main(string[] args){
+            int JmlArray;
+            Console.WriteLine("=============================================");
+            Console.Write("Silahkan Input Banyak Junlah Array :");
+            JmlArray = Convert.ToInt16(Console.ReadLine());
+            // menentukan nilai inputan string
+            string? [] NamaTeman = new string[JmlArray];
+            // menentukan inputan data menjadi array
+            for(int nAwal=0; nAwal < JmlArray; nAwal++){
+                Console.Write("Silahkan Input Nama Teman Anda : ");
+                NamaTeman[nAwal] = Console.ReadLine();
+
+            }
+            Console.WriteLine("===================================================");
+            Console.WriteLine("Daftar Nama Teman Yang Sudah Diinput :");
+            for(int nIndex = 0; nIndex < JmlArray; nIndex++){
+                System.Console.Write("Nama Ke Indek : " + nIndex); 
+                Console.WriteLine("    Daftar Nama Teman : "  + NamaTeman[nIndex]);
+            }
+            Console.WriteLine("=====================================================");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ harike5.1/tugaslat1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TipeArray{
+    class Array{
+        static void UjiCobaArray(int[,] Array){
+            for (int nAwal = 0; nAwal < Array.GetLength(0); nAwal++){
+                for (int nAkhir = 0; nAkhir < Array.GetLength(1); nAkhir++){
+                    System.Console.WriteLine ("Data Index Elemen Ke : ({0} , {1}, {2}) = {2}" , nAwal, nAkhir, Array[nAwal, nAkhir]);
+                }
+        
+            }
+        }
+        static void Main(string[] args){
+            UjiCobaArray(new int[,]{{1,5,7},{4,3,9},{7,1,3},{2,8,4},{3,3,4},{7,4,3},{3,1,3}});
+            System.Console.WriteLine("Tekan Bebas Untuk Keluar Halaman Kerja ! ");
+            System.Console.ReadKey();
+        }
+    }
+}
